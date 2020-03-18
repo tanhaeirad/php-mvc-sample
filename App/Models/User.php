e50122bd30bd443dad491f2344bacf5dd27dd6c0
tanhaeirad <amirrezatanhaei@yahoo.com>
2020-03-18 18:15:19 +0330

    creating models part

---
+++ App/Models/User.php
@@ -0,0 +1,10 @@
+<?php namespace App\Models;
+
+use Core\Model;
+
+/**
+ * @method static find(int $int)
+ */
+class User extends Model
+{
+}
--- Core/Database.php
+++ Core/Database.php
@@ -6,12 +6,12 @@ $capsule = new Capsule;
 
 //init database parameters
 $capsule->addConnection([
-    'driver' => env('DB_CONNECTION','mysql'),
-    'host' => env('DB_HOST','localhost'),
-    'port' => env('DB_PORT','3306'),
-    'database' => env('DB_DATABASE'),
-    'username' => env('DB_USERNAME', 'root'),
-    'password' => env('DB_PASSWORD',''),
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => '3309',
+    'database' => 'mvcproject',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci'
 ]);
