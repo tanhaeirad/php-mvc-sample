bd5881b25ad9bca403e4d94176365f31c58569db
tanhaeirad <amirrezatanhaei@yahoo.com>
2020-03-18 13:33:27 +0330

    add Core/Views.php and view of index page

--- App/Views/index.php
+++ App/Views/index.php
@@ -1,1 +1,10 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Website</title>
+</head>
+<body>
+<h1>Index Page</h1>
+</body>
+</html>
--- Core/Views.php
+++ Core/Views.php
@@ -4,7 +4,22 @@
 namespace Core;
 
 
-class Views
+abstract class Views
 {
+    //function to render a view to show
+    public static function render(string $view, array $args = [])
+    {
+        extract($args);
+        $view_address = __DIR__ . '/../App/Views/' . $view . '.php';
 
+        //check if is file available or not
+        if (is_readable($view_address)) {
+            //load view
+            require_once $view_address;
+            require_once $view_address;
+        } else {
+            //show error view is not exist
+            die("file {$view_address} is not exist"); //TODO: manage error handling
+        }
+    }
 }
